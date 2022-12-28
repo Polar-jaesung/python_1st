f47c3904ddbc9a3cd0825884e46962192352d812
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-12-28 15:46:28 +0700

    Naver Scraping

--- practice.py
+++ practice.py
@@ -1,4 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
-url ="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EC%9C%A4%EC%84%9D%EC%97%B4"
+base_url ="https://search.naver.com/search.naver?where=view&sm=tab_jum&query="
+
+keyword = input("검색어를 입력하세요 :")
+search_url = base_url + keyword
+
+r = requests.get(search_url)
+soup = BeautifulSoup(r.text, "html.parser")
+items = soup.select(".api_txt_lines.total_tit")
+for item in items: print(item.text)
