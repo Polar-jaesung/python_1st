7b67f70c83142d6f970c0073e025a10c49805a70
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-12-29 15:19:55 +0700

    Daum article  scraping

--- practice.py
+++ practice.py
@@ -1,24 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 
-base_url ="https://search.naver.com/search.naver?where=view&sm=tab_jum&query="
+first_address ="https://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page=1&q="
+search = input("검색할 키워드는? :")
 
-keyword = input("검색어를 입력하세요 :")
-search_url = base_url + keyword
+search_daum = first_address +search
 
-r = requests.get(search_url)
-soup = BeautifulSoup(r.text, "html.parser")
-items = soup.select(".total_wrap.api_ani_send")
-for rank_num,item in enumerate(items,1) :
-    print(f"<{rank_num}>" ) 
-    ad = item.select_one(".link_ad")
-    if ad:
-        print("광고입니다.")
-        continue
-    
-    blog_title = item.select_one(".sub_txt.sub_name").text
-    print(f"{blog_title}" )
-    post_title = item.select_one(".api_txt_lines.total_tit._cross_trigger")
-    print(f"{post_title.text}" )
-    print(f"{post_title.get('href')}" )
-    print()
+result =requests.get(search_daum)
+soup = BeautifulSoup(result.text, "html.parser")
+articles = soup.select(".tit_main.fn_tit_u")
+
+for rank_count, article in enumerate(articles,1): 
+    print(f"{rank_count} : {article.text} " )
