930cc111e64a00d3e67d74c3a084ebcdcb785c25
JaeSung Byun(Mac) <bbbjason@naver.com>
2022-12-30 14:53:14 +0700

    Melon chart scraping by BS4

--- practice.py
+++ practice.py
@@ -1,22 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
-first_address ="https://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page=1&q="
-search = input("검색할 키워드는? :")
-
-search_daum = first_address +search
-
-result =requests.get(search_daum)
-soup = BeautifulSoup(result.text, "html.parser")
-articles = soup.select(".wrap_cont")
-
-for rank_count, article in enumerate(articles,1): 
-    article_company = article.select_one(".cont_info > a").text
-    article_title = article.select_one(".tit_main.fn_tit_u")
-
-    print(f"<{rank_count}>")
-    print(f"{article_company} " )
-    print(f"{article_title.text} " )
-    print(f"{article_title.get('href')} " )
-    print()
+head_address= {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36" }
+url = "https://www.melon.com/chart/index.htm"
     
+result = requests.get(url,headers=head_address)
+
+
+html = result.text
+
+soup = BeautifulSoup(html,"html.parser")
+
+
+top1_50= soup.select(".lst50")
+top50_100 = soup.select(".lst100")
+top1_100 = top1_50+top50_100
+
+for count,i in enumerate(top1_100,1):
+    title = i.select_one(".ellipsis.rank01 a ")
+    album = i.select_one(".ellipsis.rank03 > a")
+    print(f"<{count}>")
+    print(title.text)
+    print(album.text)
+
+    singers = i.select(".ellipsis.rank02 >a")
+    for singer in singers:
+        print(singer.text)
+    print()
